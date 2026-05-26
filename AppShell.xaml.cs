6dc8603dc9d2ab275fc446742078d0ed6fb625c6
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2026-05-26 09:30:49 -0300

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppAlbumFigurinhas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppAlbumFigurinhas")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppAlbumFigurinhas.Pages")]
