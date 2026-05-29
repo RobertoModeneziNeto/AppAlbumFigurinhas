4fa7f1f1b44f3180bc90f2d78d11f701ff13da82
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2026-05-28 21:50:09 -0300

    create pacote figurinha e caminho de imagens

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -14,10 +14,35 @@ namespace AppAlbumFigurinhas
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            CopiarImagensJogadores();
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
             return builder.Build();
+        }
+
+        static void CopiarImagensJogadores()
+        {
+            try
+            {
+                var destino = Path.Combine(AppContext.BaseDirectory, "Imagens");
+                if (!Directory.Exists(destino))
+                    Directory.CreateDirectory(destino);
+
+                var origem = @"C:\Users\Roberto\Downloads\TODAS_LAS_FIGURITAS";
+
+                if (!Directory.Exists(origem)) return;
+
+                foreach (var arquivo in Directory.GetFiles(origem))
+                {
+                    var nomeArquivo = Path.GetFileName(arquivo);
+                    var caminhoDestino = Path.Combine(destino, nomeArquivo);
+                    if (!File.Exists(caminhoDestino))
+                        File.Copy(arquivo, caminhoDestino);
+                }
+            }
+            catch { }
         }
     }
 }
